c75ee6bcb64f22666c9dbcb1cb9d4b014ce3a3c3
Dan Jenkins <danjenkins@Alannas-MacBook-Air.local>
2021-05-17 08:58:48 +0100

    make sendgrid work, i think

--- sendgrid/sendgrid.go
+++ sendgrid/sendgrid.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ant0ine/go-json-rest/rest"
 
 	"go.askask.com/rt-mail/rt"
+	"github.com/sendgrid/sendgrid-go/helpers/inbound"
 )
 
 type Sendgrid struct {
@@ -18,6 +19,28 @@ func (sg *Sendgrid) GetRoutes() []*rest.Route {
 }
 
 func (sg *Sendgrid) ReceiveHandler(w rest.ResponseWriter, r *rest.Request) {
-	w.WriteHeader(500)
-	w.WriteJson(struct{ Error string }{Error: "not implemented"})
+	fmt.Printf("POST to '%s': %#v\n\n", r.URL.String(), r)
+
+	parsedEmail, err := Parse(request)
+	if err != nil {
+		fmt.Printf("Could not parse email: %s", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	fmt.Printf("Got a message from '%s' to '%s'", parsedEmail.Headers["From"], parsedEmail.Headers["To"])
+    
+	err = sp.RT.Postmail(parsedEmail.Headers["To"], parsedEmail.rawRequest)
+	if err != nil {
+		fmt.Printf("post error: %s", err)
+		if err, ok := err.(*rt.Error); ok {
+			if err.NotFound {
+				w.WriteHeader(404)
+				return
+			}
+		}
+		w.WriteHeader(503)
+	}
+
+	w.WriteHeader(204)
 }
